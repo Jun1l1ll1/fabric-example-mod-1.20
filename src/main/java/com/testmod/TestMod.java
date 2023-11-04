c2bcb548e82bf98ebc96f002f0b7c334d655bc6d
Jun1l1ll1 <jujoa007@osloskolen.no>
2023-11-04 15:24:17 +0100

    Added 2 items

--- src/main/java/com/testmod/TestMod.java
+++ src/main/java/com/testmod/TestMod.java
@@ -5,12 +5,16 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.testmod.item.ModItems;
+
+
 public class TestMod implements ModInitializer {
-    private static final String MOD_ID = "testmod";
+    public static final String MOD_ID = "testmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 		
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/com/testmod/item/ModItems.java
@@ -0,0 +1,34 @@
+package com.testmod.item;
+
+import com.testmod.TestMod;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item SHRED = registerItem("shred", new Item(new FabricItemSettings()));
+    public static final Item HEART = registerItem("heart", new Item(new FabricItemSettings()));
+
+    public static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(SHRED);
+        entries.add(HEART);
+    }
+
+    
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(TestMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TestMod.LOGGER.info("Registering Mod Items for " + TestMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
+    }
+}
